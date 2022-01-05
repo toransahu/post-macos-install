800ae7003aa4d47b9532b65eba88889a797705c3
Toran Sahu <toran.sahu@yahoo.com>
2022-01-05 11:03:49 +0530

    Create install_go.sh

---
+++ install_go.sh
@@ -0,0 +1,23 @@
+#! /bin/sh
+#
+# install_go.sh
+# Copyright (C) 2022 Toran Sahu <toran.sahu@yahoo.com>
+#
+# Distributed under terms of the MIT license.
+#
+
+VERSION=1.17.5
+
+# uninstall existing version
+sudo rm -rf /usr/local/go
+
+mkdir -p ~/.temp
+cd ~/.temp
+wget https://dl.google.com/go/go$VERSION.darwin-amd64.tar.gz
+sudo tar -C /usr/local -xzf go$VERSION.darwin-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+# source ~/.profile
+# echo "export PATH=\$PATH:/usr/local/go/bin" >> ~/.profile
+
+# for personal repos
+git config --global url."git@github.com:".insteadOf "https://github.com/"
