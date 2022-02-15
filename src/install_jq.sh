d6270e3da30afa73a95eec4be4358e958676422a
Toran Sahu <toran.sahu@yahoo.com>
2022-02-15 16:55:41 +0530

    //

---
+++ src/install_jq.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+#
+# install_jq.sh
+# Copyright (C) 2022 Toran Sahu <toran.sahu@yahoo.com>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+brew install jq
---
+++ src/install_rectangle.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#
+# install_rectangle.sh
+# Copyright (C) 2022 Toran Sahu <toran.sahu@yahoo.com>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+# Keyboard binding for positioning windows
+
+brew install --cask rectangle
