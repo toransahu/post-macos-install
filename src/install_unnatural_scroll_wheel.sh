916263b991cf4bf2fb8d1430ae4351672dd24b54
Toran Sahu <toran.sahu@yahoo.com>
2022-02-16 10:43:06 +0530

    //

---
+++ src/install_unnatural_scroll_wheel.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#
+# install_unnatural_scroll_wheel.sh
+# Copyright (C) 2022 Toran Sahu <toran.sahu@yahoo.com>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+# ref: https://github.com/ther0n/UnnaturalScrollWheels
+
+brew install --cask unnaturalscrollwheels
+
+# run at startup: https://github.com/ther0n/UnnaturalScrollWheels/blob/master/RunAtLogin.md
