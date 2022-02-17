e191030740046e16c69f99239f16f9ac606b0daa
Toran Sahu <toran.sahu@yahoo.com>
2022-02-17 21:57:19 +0530

    //

---
+++ src/install_git_delta.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+#
+# install_git_delta.sh
+# Copyright (C) 2022 Toran Sahu <toran.sahu@yahoo.com>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+brew install git-delta
