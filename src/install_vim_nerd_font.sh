7587ec22cca142789e04e45941e9bbdc1763de12
Toran Sahu <toran.sahu@yahoo.com>
2022-02-21 22:40:29 +0530

    nerd-font

---
+++ src/install_vim_nerd_font.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#
+# install_vim_nerd_font.sh
+# Copyright (C) 2022 Toran Sahu <toran.sahu@yahoo.com>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+brew tap homebrew/cask-fonts
+brew install --cask font-meslo-lg-nerd-font
+brew install --cask font-fira-mono-nerd-font
+brew install --cask font-fira-code-nerd-font
