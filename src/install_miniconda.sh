16f5e2e1c36337fe01ed665ab5a75696af0d2a69
Toran Sahu <toran.sahu@yahoo.com>
2022-02-23 10:42:45 +0530

    //

--- src/install_miniconda.sh
+++ src/install_miniconda.sh
@@ -6,15 +6,19 @@
 # Distributed under terms of the MIT license.
 #
 
+PY_VER=38
+CONDA_VER=4.11.0
+VERSION=py"$PY_VER"_"$CONDA_VER"
+
 install_using_sh(){
-	curl -fsSL https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh | bash -s -- -b -u
+	curl -fsSL https://repo.anaconda.com/miniconda/Miniconda3-$VERSION-MacOSX-x86_64.sh | bash -s -- -b -u
 }
 
 install_using_pkg(){
 	mkdir -p ~/.temp && cd ~/.temp
-	curl https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.pkg -o Miniconda3-latest-MacOSX-x86_64.pkg
+	curl https://repo.anaconda.com/miniconda/Miniconda3-$VERSION-MacOSX-x86_64.pkg -o Miniconda3-$VERSION-MacOSX-x86_64.pkg
 	# sudo installer -pkg Miniconda3-latest-MacOSX-x86_64.pkg -target /
-	installer -pkg Miniconda3-latest-MacOSX-x86_64.pkg -target CurrentUserHomeDirectory 
+	installer -pkg Miniconda3-$VERSION-MacOSX-x86_64.pkg -target CurrentUserHomeDirectory 
 }
 
 install_using_pkg
