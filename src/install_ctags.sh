e7a834f2bef66d1c446447759b5501f041f23d5a
Toran Sahu <toran.sahu@yahoo.com>
2022-03-18 16:28:37 +0530

    ctags

---
+++ src/install_ctags.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#
+# install_ctags.sh
+# Copyright (C) 2022 Toran Sahu <toran.sahu@yahoo.com>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+# install exuberant ctags (replace default BSD ctags)
+brew install ctags
