c0680aa9a9a330f3c2ab38c0afe0f149a11d052c
Toran Sahu <toran.sahu@yahoo.com>
2022-03-18 16:03:31 +0530

    flink

---
+++ src/install_apache_flink.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#
+# install_apache_flink.sh
+# Copyright (C) 2022 Toran Sahu <toran.sahu@yahoo.com>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+brew install apache-flink
+# usage: 
+# $ flink -h
