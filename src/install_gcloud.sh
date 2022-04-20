92a9af5ea542130d1751357ea463533a293d7a91
Toran Sahu <toran.sahu@yahoo.com>
2022-04-20 16:06:30 +0530

    install gcloud

---
+++ src/install_gcloud.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+#
+# install_gcloud.sh
+# Copyright (C) 2022 Toran Sahu <toran.sahu@yahoo.com>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+curl https://sdk.cloud.google.com | bash
