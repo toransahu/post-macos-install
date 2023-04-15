4c27c13237726c2c2af7073e4082bf2a4b2b8769
Toran Sahu <toran.sahu@yahoo.com>
2023-04-15 16:17:15 +0530

    Install stretchly - Eye blink reminder

---
+++ src/install_stretchly.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+#
+# install_stretchly.sh
+# Copyright (C) 2023 Toran Sahu <toran.sahu@yahoo.com>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+brew update && brew install --cask stretchly
