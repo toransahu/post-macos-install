e429ce618adf125e022243af315ed5c541803af6
Toran Sahu <toran.sahu@yahoo.com>
2023-04-15 16:19:00 +0530

    Install sdk manager

---
+++ src/install_sdk.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+#
+# install_sdk.sh
+# Copyright (C) 2022 Toran Sahu <toran.sahu@yahoo.com>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+curl -s "https://get.sdkman.io" | bash
