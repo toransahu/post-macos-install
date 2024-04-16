aff36514e8c760bc03d0ac61f1ad9d6c1460423e
Toran Sahu <toran.sahu@yahoo.com>
2024-04-16 20:49:25 +0530

    //

---
+++ src/install_vim_shfmt_prereq.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+#
+# install_vim_shfmt_prereq.sh
+# Copyright (C) 2024 Toran Sahu <toran.sahu@yahoo.com>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+go install mvdan.cc/sh/v3/cmd/gosh@latest
