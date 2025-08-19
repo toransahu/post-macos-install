d9099aaccfb7a9c9287b138bf37a24639bd9517d
Toran Sahu <toran.sahu@yahoo.com>
2025-08-19 21:53:38 +0530

    //

--- src/install_vim.sh
+++ src/install_vim.sh
@@ -21,24 +21,38 @@ make distclean && make clean
 # cd ..
 
 # Configure
-
 LDFLAGS="-rdynamic" ./configure \
   --enable-multibyte \
-  # --enable-pythoninterp=dynamic \
   --enable-python3interp \
   --with-python3-config-dir=$HOME/.pyenv/versions/3.8.12/lib/python3.8/config-3.8-darwin/ \
   --enable-cscope \
   --enable-largefile \
   --enable-perlinterp=dynamic \
-  # --enable-rubyinterp=dynamic \
-  # --with-ruby-command=/usr/local/bin/ruby \
   --enable-gui=auto \
   --with-features=huge \
-  # --with-x \
   --enable-fontset \
   --disable-netbeans \
   --with-compiledby="Toran Sahu <toran.sahu@yahoo.com>" \
   --enable-fail-if-missing 
+
+
+# LDFLAGS="-rdynamic" ./configure \
+#   --enable-multibyte \
+#   # --enable-pythoninterp=dynamic \
+#   --enable-python3interp \
+#   --with-python3-config-dir=$HOME/.pyenv/versions/3.8.12/lib/python3.8/config-3.8-darwin/ \
+#   --enable-cscope \
+#   --enable-largefile \
+#   --enable-perlinterp=dynamic \
+#   # --enable-rubyinterp=dynamic \
+#   # --with-ruby-command=/usr/local/bin/ruby \
+#   --enable-gui=auto \
+#   --with-features=huge \
+#   # --with-x \
+#   --enable-fontset \
+#   --disable-netbeans \
+#   --with-compiledby="Toran Sahu <toran.sahu@yahoo.com>" \
+#   --enable-fail-if-missing 
 
 # Install
 sudo make && sudo make install
